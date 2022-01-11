98fbd28c05d1bb290a233c1e3cb074055070acb5
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-01-10 23:12:37 -0500

    Time: 50 ms (64.41%), Space: 14.3 MB (32.31%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        int_num1 = int(num1)
+        int_num2 = int(num2)
+        multiply = int_num1 * int_num2
+        return str(multiply)
+        
