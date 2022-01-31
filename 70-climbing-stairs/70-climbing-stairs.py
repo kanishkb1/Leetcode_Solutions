f30c109218d6b9952efdfe287441ec4f4ed7de0a
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-01-30 23:35:09 -0500

    Time: 45 ms (29.77%), Space: 13.9 MB (98.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -3,11 +3,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n == 1:
             return 1
+        if n==2:
+            return 2
         res = [0 for i in range(n)]
         res[0], res[1] = 1, 2
         for i in range(2, n):
             res[i] = res[i-1] + res[i-2]
         return res[-1]
-    #Space Complexity-0(n)
-    #Time complexity- O(n)
+    #Space Complexity-O(n)
+    #Time complexity- O(n*N)
             
