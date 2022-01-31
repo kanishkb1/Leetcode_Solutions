ef3d341d1221f36c9d9fb7a3bda07ba6d80b1904
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-01-30 23:38:00 -0500

    Time: 32 ms (70.34%), Space: 13.9 MB (98.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,15 +1,19 @@
 from functools import lru_cache
 class Solution:
     def climbStairs(self, n: int) -> int:
+        #EDGE CASES
         if n == 1:
             return 1
         if n==2:
             return 2
+        
+        #recursion using an array    
         res = [0 for i in range(n)]
         res[0], res[1] = 1, 2
         for i in range(2, n):
             res[i] = res[i-1] + res[i-2]
         return res[-1]
+    
     #Space Complexity-O(n)
     #Time complexity- O(n*N)
             
