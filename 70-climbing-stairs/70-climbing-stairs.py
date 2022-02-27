6db4e7792446741f89a70baf96ea3c3a72706f95
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-02-27 09:09:58 -0500

    Time: 39 ms (58.98%), Space: 13.9 MB (82.27%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -16,3 +16,5 @@ class Solution:
         for i in range(2,n):
             current[i] = current[i-1] + current[i-2]
         return current[-1]
+    #Space complexity- O(n)
+    #Time complexity- O(n*N)
