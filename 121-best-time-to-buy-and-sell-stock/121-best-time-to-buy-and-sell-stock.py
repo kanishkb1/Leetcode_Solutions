c9fbdf43db4be1ca14f9e7f733f15a7017043613
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-02-28 18:39:07 -0500

    Time: 1613 ms (36.63%), Space: 25.1 MB (27.82%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -6,5 +6,3 @@ class Solution:
             max_profit = max(max_profit, i-min_price)
             min_price = min(min_price,i)
         return max_profit
-        #Time complexity-O(n)
-        #Space complexity- O(1)
