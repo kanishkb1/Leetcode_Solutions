f319db12f0c6bf7e85fe2c618021f549cde280cc
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-04-01 09:13:02 -0400

    Time: 170 ms (66.89%), Space: 15.2 MB (37.98%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,12 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n =len(nums)
-        nums = sorted(nums)
-        return sum(range(0,n+1)) - sum(nums)
-    
-    #Time complexity- O(1)
-    #Space complexity- O(n)
-    
-    
-        
-        
+        n=len(nums)
+        return sum(range(n+1)) - sum(sorted(nums))
+       
