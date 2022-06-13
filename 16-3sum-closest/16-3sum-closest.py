da76697127e323913dafa1e41d217a6f92ac06d5
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-06-13 09:23:55 -0400

    Time: 335 ms (61.11%), Space: 14 MB (67.64%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        diff = float("inf")
+        n=len(nums)
+    
+    
+        for i in range(n-2):
+           
+            pointer1=i+1
+            pointer2=n-1
+            
+            while pointer1<pointer2:
+                total = nums[i] + nums[pointer1] + nums[pointer2]
+                
+                if total==target:
+                    return total
+                
+                if abs(total-target) <= diff:
+                    diff = abs(total-target)
+                    answer = total
+                    
+                if total<target:
+                    pointer1+=1
+                else:
+                    pointer2-=1
+                    
+        return answer
+    
+ 
+        return ans
