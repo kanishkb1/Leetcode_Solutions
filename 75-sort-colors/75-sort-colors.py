3d0b5ed2466a781df8789272e4a114ab1968ffe9
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-06-14 16:17:32 -0400

    Time: 33 ms (92.95%), Space: 13.8 MB (62.78%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,6 +1,22 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None: 
-        n=len(nums)
         
-        nums.sort()
-        return nums
+        n = len(nums)
+        #creating two pointers
+        low = 0
+        i=0
+        
+        high = n-1
+        while i<=high:
+            if nums[i]==0:
+                nums[i],nums[low]=nums[low],nums[i]
+                i+=1
+                low+=1
+        
+            elif nums[i]==1:
+                i+=1
+            else:
+                nums[i],nums[high]=nums[high],nums[i]
+                high-=1
+                
+     
