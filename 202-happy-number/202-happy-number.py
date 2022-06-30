7a15a7c27398873390a868fea800e50a0de3d6ca
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-06-29 23:02:25 -0400

    Time: 33 ms (95.71%), Space: 13.9 MB (14.19%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+	    slow = self.squared(n)
+	    fast = self.squared(self.squared(n))
+
+	    while slow!=fast and fast!=1:
+		    slow = self.squared(slow)
+		    fast = self.squared(self.squared(fast))
+
+	    return fast==1
+
+    def squared(self, n):
+	    result = 0
+	    while n>0:
+		    last = n%10
+		    result += last * last
+		    n = n//10
+	    return result
+   
+            
+        
+        #Time complexity- O (Log N)
+        #Space complexity-p O(1)
+        
+        
+            
+        
