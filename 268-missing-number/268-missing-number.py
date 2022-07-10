cb150db335b7654268733a1ba953dd66e529f519
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-07-10 11:29:02 -0400

    Time: 234 ms (38.61%), Space: 15.2 MB (33.14%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n=len(nums)
-        return sum(range(n+1)) - sum(sorted(nums))
-       #Runtime complexity- O(1)
-       #Space complexity- O(n)
+        n = len(nums)
+        #x1 -> represents XOR of all values from 1 to n
+        x1 =1
+        for i in range(2,n+1):
+            x1 = x1 ^ i
+        #x2 -> XOR of all values in nums    
+        x2 = nums[0]
+        for i in range(1,n):
+            x2 = x2 ^ nums[i]
+        #    
+        return x1 ^ x2
+    
+    #Time complexity- O(n)
+    #Space complexitry- O(1)
