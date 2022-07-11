e8ce70983e6fa584a44dfe8d3baeeac84f507958
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-07-11 13:29:54 -0400

    Time: 44 ms (56.73%), Space: 13.9 MB (9.51%) - LeetHub

---
+++ 476-number-complement/476-number-complement.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        mask =0
+        temp = num
+        
+        while temp:
+            mask = (mask<<1) | 1
+            temp = temp >> 1
+        return mask ^ num
+        
