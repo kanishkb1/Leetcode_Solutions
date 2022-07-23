b50a71d6381de4722209c741853b71eeaec36ad4
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-07-23 10:07:44 -0400

    Time: 162 ms (84.64%), Space: 18.1 MB (84.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -4,15 +4,19 @@ class Solution:
         if len(intervals)<2:
             return intervals
         
+        #Constants for start and end of time intervals
         START,END=0,1
         result=[]
 
+        #makke all intervals sorted on start and end time intervals in ascending order
         intervals.sort( key=lambda x: (x[START],x[END] ) )
         
+    
         for interval in intervals:
+            #no overlapping 
             if not result or (result[-1][END] < interval[START]):
                 result.append(interval)
-                
+            #merge with previous interval    
             else:
                 result[-1][END] = max(result[-1][END],interval[END])
                 
