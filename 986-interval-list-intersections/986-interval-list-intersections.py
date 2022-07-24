3dae56b5266151c8c1b8fcd2363bbc970b6ddf0a
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-07-24 13:07:06 -0400

    Time: 270 ms (28.07%), Space: 14.9 MB (49.91%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -8,8 +8,9 @@ class Solution:
         while i < len(firstList) and j < len(secondList):
             
             start = max(firstList[i][0],secondList[j][0])
-            end = min(firstList[i][1],secondList[j][1])
             
+          
+            end = min(firstList[i][1],secondList[j][1])
             if start <= end:
                 result.append([start,end])
             
