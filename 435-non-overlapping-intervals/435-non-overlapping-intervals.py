c67eab097beddac3a01e6b371fd6705c943808b8
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-07-24 14:33:50 -0400

    Time: 2571 ms (21.88%), Space: 52.1 MB (84.61%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -8,11 +8,10 @@ class Solution:
         current = intervals[0]
         
         for i in range(n):
-            
             if intervals[i][0] >= current[1]:
-                print(intervals[i][0],current[1])
-                ans+=1
+                
                 current = intervals[i]
+                ans+=1
         return n - ans
     
     #Time complexity- O(N)
