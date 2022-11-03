5c9c3c408a90b88bb1a8dd0ac0bb512d5977fefb
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-11-03 19:15:13 -0400

    Time: 87 ms (5.48%), Space: 13.9 MB (81.73%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pointer1, pointer2 = 0,0
+ 
+        k = ''
+        pointer1 = 0 
+        pointer2 = 0
+        
+        while pointer1 < len(s) and pointer2 < len(t):
+            if s[pointer1] == t[pointer2]:
+                k += s[pointer1]
+                pointer1 += 1
+            pointer2 += 1
+        return k == s 
+ 
