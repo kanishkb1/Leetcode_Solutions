a2b84db02053cdbd0eefd53b2697fd5a98a43fb5
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2022-11-02 22:54:52 -0400

    Time: 8477 ms (5.09%), Space: 14.9 MB (79.80%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        #initialization of two pointer
+        #
         left=0
         n=len(nums)
         right=n - 1
         
-        #left will be incremented and right will be decremented.
+  #
         while left <= right:
             if nums[left] + nums[right] == target:
                 return [left,right]
